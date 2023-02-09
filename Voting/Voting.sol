652a412dde3d1e99d461c0374e59e236da5b1e65
sjouany <sylvain.jouany@gmail.com>
2023-02-09 13:35:21 +0100

    init struct and functions

---
+++ Voting/Voting.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.8.18;
+
+import import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+
+  // Enumeration to manage differents sates of voting
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+
+    event VoterRegistered(address voterAddress); 
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint proposalId);
+    event Voted (address voter, uint proposalId);
+
+
+// fonction getWinner to implement (best in term of costing than store an attribute like winningProposalId)
